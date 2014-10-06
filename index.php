147ee1e2b938b29f140577fc156064220080cf63
MarkNewcomb <marknewcomb@Marks-MacBook-Pro.local>
2014-10-05 18:35:23 -0600

    mn starting up!

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>The Unofficial Hot Rod User's Guide</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+
+  <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+<?php include("inc/inc_header.php"); ?>
+<h1>Hi There! HTML Content in your face!</h1>
+<?php include("inc/inc_footer.php"); ?>
+<!--   <script src="js/scripts.js"></script> -->
+</body>
+</html>
