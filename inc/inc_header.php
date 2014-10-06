66d15b942beb452cfe92a1f75c1df4f2404e3799
MarkNewcomb <marknewcomb@Marks-MacBook-Pro.local>
2014-10-05 18:36:29 -0600

    mn changed h1

--- inc/inc_header.php
+++ inc/inc_header.php
@@ -1,4 +1,4 @@
  <table width="100%" style="border: 0">
-         <tr><td><h1>Coast City Computers</h1></td>
+         <tr><td><h1>The Unofficial Fender Hot Rod Deluxe User's Guide</h1></td>
          <td style="text-align: right"><strong>Buy Online or
          Call 1-800-555-1212</strong></td></tr></table><hr />
