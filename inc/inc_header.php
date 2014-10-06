fab388ea8a19ff115fb5a0e4e9b1f5ddff7c8118
MarkNewcomb <marknewcomb@Marks-MacBook-Pro.local>
2014-10-06 16:29:43 -0600

    mn

--- inc/inc_header.php
+++ inc/inc_header.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-
+  <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>The Unofficial Hot Rod User's Guide</title>
   <meta name="description" content="The HTML5 Herald">
   <meta name="author" content="SitePoint">
