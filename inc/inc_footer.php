05d45ae66d7369dcf9053f6f884ef3ebe56d183c
MarkNewcomb <marknewcomb@Marks-MacBook-Pro.local>
2015-01-26 10:04:24 -0700

    mn only loads menu when 719 and under

--- inc/inc_footer.php
+++ inc/inc_footer.php
@@ -3,9 +3,15 @@
 <p class="notice">Web Design and Layout &copy; <?php echo date("Y") ?> by Mark Newcomb. All Rights Reserved.</p>
 </div><!--end doc container-->
 <script>
-	$(function(){
+	
+	function checkPosition() {
+    if (window.matchMedia('(max-width: 719px)').matches) {
+       	$(function(){
 		$('#menu').slicknav();
 	});
+    } 
+}
+checkPosition();
 </script>
 </footer>
 </body>
