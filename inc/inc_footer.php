dddb72b014281383486360675ec9079a26cbc3aa
MarkNewcomb <marknewcomb@Marks-MacBook-Pro.local>
2015-03-01 14:28:43 -0700

    mn
    added footer nav links, required more formatting than I thought but oh
    well

--- inc/inc_footer.php
+++ inc/inc_footer.php
@@ -1,5 +1,6 @@
 <footer>
 <div class="doc_container">
+<nav class="bottom-nav"><a href="/">Home</a>&nbsp; | &nbsp;&nbsp;<a href="/howtoarticles.php/">How-To Articles</a>&nbsp; | &nbsp;&nbsp;<a href="/faq.php">Faq</a></nav>
 <p class="notice">Web Design and Layout &copy; <?php echo date("Y") ?> by Mark Newcomb. All Rights Reserved.</p>
 </div><!--end doc container-->
 <script>
