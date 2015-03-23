7878d0e64a0abfa9b3cd40b54ec4f474f97888ad
MarkNewcomb <marknewcomb@Marks-MacBook-Pro.local>
2015-03-22 19:16:36 -0600

    mn added two buttons, hero element

--- index.php
+++ index.php
@@ -1,39 +1,18 @@
 <?php include("inc/inc_header.php"); ?>
-<article class="book doc_container">
- <style>
-  section { border: double medium; margin: 2em; }
-  section.chapter h1 { font: 2em Roboto, Helvetica Neue, sans-serif; }
-  section.appendix h1 { font: small-caps 2em Roboto, Helvetica Neue, sans-serif; }
- </style>
- <header>
-   <h2><a name="top"></a>My Hot Rod Website</h2>
-   <p>A sample with not much content</p>
-  <p><small>Published by Dummy Publicorp Ltd.</small></p>
- </header>
-  
- <section class="chapter">
-  <h3>My First Chapter</h3>
-  <p>This is the first of my chapters. It doesn't say much.</p>
-  <p>But it has two paragraphs!</p>
- </section>
- <section class="chapter">
-  <h3>It Continues: The Second Chapter</h3>
-  <p>Bla dee bla, dee bla dee bla. Boom.</p>
- </section>
- <section class="chapter">
-  <h3>Chapter Three: A Further Example</h3>
-  <p>It's not like a battle between brightness and earthtones would go
-  unnoticed.</p>
-  <p>But it might ruin my story.</p>
- </section>
- <section class="appendix">
-  <h3>Appendix A: Overview of Examples</h3>
-  <p>These are demonstrations.</p>
- </section>
- <section class="appendix">
-  <h3>Appendix B: Some Closing Remarks</h3>
-  <p>Hopefully this long example shows that you <em>can</em> style
-  sections, so long as they are used to indicate actual sections.</p>
- </section>
+<div class="hero">
+	<figure class="content100">
+	<img src="img/hero_element.png">
+	</figure>
+	<div class="inside-hero">
+		<a class="button" href="#">The Articles</a>
+		<a class="button" href="faq.php">The FAQs</a>
+	</div>
+</div> <!-- end hero -->
+<div class="doc_container">
+<article>
+  <section>
+	  Welcome! Here you'll find a database of information that'll help you get the most out of your Hot Rod Deluxe. After years of searching the internet I was dissapointed that there was no real source of information on this amp—yet it remains one of the most popular in the world. For a long time building a Hot Rod Deluxe website was one of the things I wanted to do, and felt needed to be done. Listening to techs, experimenting with electronics, and talking with other owners inspired me to collect a large source of mods, tips, tricks, fixes, and frequently asked questions to help you on your way to finding *your* tone. 
+  </section>
 </article>
+</div> <!-- end div doc container -->
 <?php include("inc/inc_footer.php"); ?>
