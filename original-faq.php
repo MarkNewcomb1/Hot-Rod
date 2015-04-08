b7951b13f33ff33d52a0978e2853247c14c09724
MarkNewcomb <marknewcomb@Marks-MacBook-Pro.local>
2015-04-07 22:14:28 -0600

    mn added faq questions

--- original-faq.php
+++ original-faq.php
@@ -2,17 +2,81 @@
 <script src="js/faq.js"></script>
 <div class="doc_container">
 <article>
+		<header><h1>The Original FAQ</h1></header>
 	<div class="main">
-	<header><h1>The Original FAQ</h1></header>
-	<h2 class="question">I've heard that JavaScript is the long-lost fountain of youth. Is this true?</h2>
+	<h2 class="question">I own a (Hot Rod Deville, Blues Junior, Blues Deluxe, Blues Deville, Pro Junior) can I use the information on this site?</h2>
 	    <div class="answer">
 	        <p>Why, yes it is! Studies prove that learning JavaScript freshens the mind and extends life span by several hundred years. (Note: some scientists disagree with these claims.)</p>
 	      </div>
-	      <h2 class="question">Can JavaScript really solve all of my problems?</h2>
+	      <h2 class="question">Are the Hot Rod Deluxe/Deville All Tube?</h2>
 	      <div class="answer">
 	        <p>Why, yes it can! It's the most versatile programming language ever created and is trained to provide financial management advice, life-saving CPR, and even to take care of household pets.</p>
 	      </div>
-	      <h2 class="question">Is there nothing JavaScript <em>can&#8217;t</em> do?</h2>
+	      <h2 class="question">What about the More Drive?</h2>
+	      <div class="answer">
+	        <p>Why, no there isn&#8217;t! It&#8217;s even able to write its own public relations-oriented Frequently Asked Questions pages. Now that&#8217;s one smart programming language!</p>
+      	</div>
+      	 <h2 class="question">What's the difference between the Hot Rod Deluxe and Deville?</h2>
+	      <div class="answer">
+	        <p>Why, no there isn&#8217;t! It&#8217;s even able to write its own public relations-oriented Frequently Asked Questions pages. Now that&#8217;s one smart programming language!</p>
+      	</div>
+      	 <h2 class="question">Why a tweed cabinet with blackface tolex and grill?</h2>
+	      <div class="answer">
+	        <p>Why, no there isn&#8217;t! It&#8217;s even able to write its own public relations-oriented Frequently Asked Questions pages. Now that&#8217;s one smart programming language!</p>
+      	</div>
+      	<h2 class="question">How do I date my Hot Rod Deluxe? (Determine when it was manufactured)</h2>
+	      <div class="answer">
+	        <p>Why, no there isn&#8217;t! It&#8217;s even able to write its own public relations-oriented Frequently Asked Questions pages. Now that&#8217;s one smart programming language!</p>
+      	</div>
+      	<h2 class="question">Which preamp tubes are V1, V2, and V3? What do they do?</h2>
+	      <div class="answer">
+	        <p>Why, no there isn&#8217;t! It&#8217;s even able to write its own public relations-oriented Frequently Asked Questions pages. Now that&#8217;s one smart programming language!</p>
+      	</div>
+      	<h2 class="question">Why don't they call them the "F.A.T" Series anymore?</h2>
+	      <div class="answer">
+	        <p>Why, no there isn&#8217;t! It&#8217;s even able to write its own public relations-oriented Frequently Asked Questions pages. Now that&#8217;s one smart programming language!</p>
+      	</div>
+      	<h2 class="question">Is my Fender amp under warranty? Should I mod?</h2>
+	      <div class="answer">
+	        <p>Why, no there isn&#8217;t! It&#8217;s even able to write its own public relations-oriented Frequently Asked Questions pages. Now that&#8217;s one smart programming language!</p>
+      	</div>
+      	<h2 class="question">How do I contact Fender for Customer/Technical Support?</h2>
+	      <div class="answer">
+	        <p>Why, no there isn&#8217;t! It&#8217;s even able to write its own public relations-oriented Frequently Asked Questions pages. Now that&#8217;s one smart programming language!</p>
+      	</div>
+      	<h2 class="question">Was a tweed version made of the Hot Rod Deluxe/Deville?</h2>
+	      <div class="answer">
+	        <p>Why, no there isn&#8217;t! It&#8217;s even able to write its own public relations-oriented Frequently Asked Questions pages. Now that&#8217;s one smart programming language!</p>
+      	</div>
+      	<h2 class="question">Were there any other versions of the Hot Rod Deluxe made?</h2>
+	      <div class="answer">
+	        <p>Why, no there isn&#8217;t! It&#8217;s even able to write its own public relations-oriented Frequently Asked Questions pages. Now that&#8217;s one smart programming language!</p>
+      	</div>
+      	<h2 class="question">What kind of speaker comes stock in my HRDx?</h2>
+	      <div class="answer">
+	        <p>Why, no there isn&#8217;t! It&#8217;s even able to write its own public relations-oriented Frequently Asked Questions pages. Now that&#8217;s one smart programming language!</p>
+      	</div>
+      	<h2 class="question">Why do the OEM Groove Tubes have different colored labels?</h2>
+	      <div class="answer">
+	        <p>Why, no there isn&#8217;t! It&#8217;s even able to write its own public relations-oriented Frequently Asked Questions pages. Now that&#8217;s one smart programming language!</p>
+      	</div>
+      	<h2 class="question">Why Hot Rod makes a loud "pop" when switching channels? Help!</h2>
+	      <div class="answer">
+	        <p>Why, no there isn&#8217;t! It&#8217;s even able to write its own public relations-oriented Frequently Asked Questions pages. Now that&#8217;s one smart programming language!</p>
+      	</div>
+      	<h2 class="question">How do I replace a blown jewel light?</h2>
+	      <div class="answer">
+	        <p>Why, no there isn&#8217;t! It&#8217;s even able to write its own public relations-oriented Frequently Asked Questions pages. Now that&#8217;s one smart programming language!</p>
+      	</div>
+      	<h2 class="question">My Hot Rod has foam around the tubes. Should I remove it?</h2>
+	      <div class="answer">
+	        <p>Why, no there isn&#8217;t! It&#8217;s even able to write its own public relations-oriented Frequently Asked Questions pages. Now that&#8217;s one smart programming language!</p>
+      	</div>
+      	<h2 class="question">Can I turn my HRDx into a head?</h2>
+	      <div class="answer">
+	        <p>Why, no there isn&#8217;t! It&#8217;s even able to write its own public relations-oriented Frequently Asked Questions pages. Now that&#8217;s one smart programming language!</p>
+      	</div>
+      	<h2 class="question">What are the differences between the Hot Rod Deluxe and Blues Deluxe? By Steve Dallman</h2>
 	      <div class="answer">
 	        <p>Why, no there isn&#8217;t! It&#8217;s even able to write its own public relations-oriented Frequently Asked Questions pages. Now that&#8217;s one smart programming language!</p>
       	</div>
